7886c69f8bf32559eb0a985e5c91242c6224ca7f
FabianAster <fabast01@gmail.com>
2024-11-19 09:27:13 +0100

    struggling with cpp standart

--- include/fish_game/NetworkClient.hpp
+++ include/fish_game/NetworkClient.hpp
@@ -3,10 +3,16 @@
 
 #include "./GameInputEvents.hpp"
 #include "./SocketManager.hpp"
+#include <concepts>
 #include <mutex>
 #include <queue>
 #include <string>
 #include <thread>
+
+template <typename T>
+concept HasFoo = requires(T t) {
+	{ t.foo() } -> std::same_as<void>;
+};
 
 class NetworkClient {
   public:
@@ -26,6 +32,10 @@ class NetworkClient {
 	std::thread workerThread;
 	std::mutex mutex;
 	std::queue<InputEvent::Event> sendQueue;
+
+	// generate id local to the network client
+	// generate ids for templates that accept objects
+	// combine ids to send to server
 
 	void run();
 };
