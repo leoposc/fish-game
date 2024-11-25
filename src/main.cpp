3cf99409fe9a24573ef721e846250772485656af
FabianAster <fabast01@gmail.com>
2024-11-25 16:56:21 +0100

    basic example works

--- src/main.cpp
+++ src/main.cpp
@@ -8,57 +8,53 @@
 
 #include <SDL2/SDL.h>
 #include <arpa/inet.h>
+#include <cereal/archives/json.hpp>
+#include <cereal/types/memory.hpp>
+#include <cereal/types/unordered_map.hpp>
+#include <cereal/types/vector.hpp>
 #include <cstring>
 #include <functional>
 #include <iostream>
-#include <netinet/in.h>
-#include <sys/socket.h>
-#include <sys/types.h>
-#include <unistd.h>
+#include <memory>
+#include <sstream>
+#include <vector>
 
-#define PORT 8080
-#define BUFFER_SIZE 1024
+class MyRecord {
+  public:
+	uint8_t x, y;
+	float z;
 
-using cG = FishEngine::ClientGame;
-using sG = FishEngine::ServerGame;
+	MyRecord() = default;
+	MyRecord(uint8_t x, uint8_t y, float z) : x(x), y(y), z(z) {}
 
-typedef void (*FuncPtr)();
+	template <class Archive>
+	void serialize(Archive &ar) {
+		ar(x, y, z);
+	}
+};
 
-cG *client = nullptr;
-sG *server = nullptr;
+class SomeData {
+  public:
+	std::vector<std::unique_ptr<MyRecord>> data;
+	std::string name;
+	double value;
+	std::vector<int> largeVector; // This member will not be serialized
 
-FuncPtr mainMenu();
-FuncPtr hostLobby();
+	SomeData() = default;
+	SomeData(std::vector<std::unique_ptr<MyRecord>> data, std::string name, double value, std::vector<int> largeVector)
+	    : data(std::move(data)), name(name), value(value), largeVector(largeVector) {}
 
-FuncPtr combat() {
-	const int FPS = 60;
-	const int frameDelay = 1000 / FPS;
-
-	u_int32_t frameStart;
-	int frameTime;
-
-	while (client->running()) {
-		frameStart = SDL_GetTicks();
-
-		client->handleEvents();
-		// server->handleEvents();
-
-		// server->update();
-		client->update();
-
-		client->render();
-
-		frameTime = SDL_GetTicks() - frameStart;
-
-		if (frameDelay > frameTime) {
-			SDL_Delay(frameDelay - frameTime);
-		}
+	template <class Archive>
+	void save(Archive &ar) const {
+		ar(data, name, value);
+		// largeVector is not serialized
 	}
 
 	return mainMenu();
 }
 
-FuncPtr hostLobby() {
+FuncPtr
+hostLobby() {
 	std::cout << "Host Lobby - not implemented yet - back to mainMenu" << std::endl;
 
 	return mainMenu();
@@ -91,12 +87,10 @@ FuncPtr joinLobby() {
 		// 		SDL_Delay(frameDelay - frameTime);
 		// 	}
 	}
-	// ======================== INIT GAME ============================
-	client->init(2);
-	server->init("map03.tmj", 2);
+};
 
-	std::cout << "====================GAME STARTED==================" << std::endl;
-	return combat();
+std::cout << "====================GAME STARTED==================" << std::endl;
+return combat();
 }
 
 FuncPtr mainMenu() {
@@ -118,18 +112,41 @@ FuncPtr mainMenu() {
 	default:
 		break;
 	}
-	return nullptr;
-}
 
-int main(int argc, char *argv[]) {
-	spdlog::set_level(spdlog::level::debug);
-	auto console = spdlog::stdout_color_mt("console");
-	auto err_logger = spdlog::stderr_color_mt("stderr");
+	int main(int argc, char *argv[]) {
+		spdlog::set_level(spdlog::level::debug);
+		auto console = spdlog::stdout_color_mt("console");
+		auto err_logger = spdlog::stderr_color_mt("stderr");
 
-	client = new cG("Fish Game Client", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 1280, 720, false);
-	server = new sG();
+		// Create an existing SomeData object with a different largeVector
+		std::vector<std::unique_ptr<MyRecord>> existingRecords;
+		existingRecords.push_back(std::make_unique<MyRecord>(7, 8, 9.0f));
+		SomeData existingData(std::move(existingRecords), "OldName", 24.24, {10, 20, 30});
 
-	mainMenu();
+		// Deserialize from the string into the existing object
+		try {
+			std::istringstream is(serializedData);
+			cereal::JSONInputArchive archive(is);
+			archive(existingData);
+		} catch (const cereal::Exception &e) {
+			std::cerr << "Deserialization error: " << e.what() << std::endl;
+			return 1;
+		}
 
-	return 0;
-}
+		// Print the values of the loaded data
+		std::cout << "Name: " << existingData.name << std::endl;
+		std::cout << "Value: " << existingData.value << std::endl;
+		for (const auto &record : existingData.data) {
+			std::cout << "x: " << static_cast<int>(record->x) << ", y: " << static_cast<int>(record->y)
+			          << ", z: " << record->z << std::endl;
+		}
+
+		// Print the largeVector to show it retains its previous value
+		std::cout << "largeVector: ";
+		for (const auto &val : existingData.largeVector) {
+			std::cout << val << " ";
+		}
+		std::cout << std::endl;
+
+		return 0;
+	}
